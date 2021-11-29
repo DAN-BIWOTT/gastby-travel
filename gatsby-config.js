e3c72fb8e50acf3abefc9d399b3107153cf1c8de
DAN <DANKIBIWOTT254@GMAIL.COM>
2021-11-30 00:55:57 +0300

    complete design

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
-    {
+    { 
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,

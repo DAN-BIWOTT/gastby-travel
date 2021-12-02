4e544fbab97a8152d186163f55a81bc428f7aa42
DAN <DANKIBIWOTT254@GMAIL.COM>
2021-12-02 23:17:44 +0300

    debugging netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,10 @@
 import * as React from 'react'
-import Hero from '../components/Hero'
-import Layout from '../components/Layout'
-import Seo from '../components/Seo'
-import Trips from '../components/Trips'
-import Testimonials from '../components/Testimonials'
-import Stats from '../components/Stats'
-import Email from '../components/Email'
+
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <Hero/>
-    <Trips heading="Our Favorite Destinations" />
-    <Testimonials />
-    <Stats/>
-    <Email/>
-  </Layout>
+<>
+  
+</>
 )
 
 export default IndexPage

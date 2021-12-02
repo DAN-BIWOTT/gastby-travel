1b17e95199e705b293654870abb846fab28d6e55
DAN <DANKIBIWOTT254@GMAIL.COM>
2021-12-02 23:13:08 +0300

    fixed padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import * as React from "react"
-import Hero from "../components/Hero"
-import Layout from "../components/Layout"
-import Seo from "../components/Seo"
-import Trips from "../components/Trips"
-import Testimonials from "../components/Testimonials"
-import Stats from "../components/Stats"
-import Email from "../components/Email"
+import * as React from 'react'
+import Hero from '../components/Hero'
+import Layout from '../components/Layout'
+import Seo from '../components/Seo'
+import Trips from '../components/Trips'
+import Testimonials from '../components/Testimonials'
+import Stats from '../components/Stats'
+import Email from '../components/Email'
 
 const IndexPage = () => (
   <Layout>
